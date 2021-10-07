671065a14022f229b1d01bac38bc01d02dc0904d
Emilse <emilsearias@gmail.com>
2021-10-07 20:35:49 -0300

    se modificaron algunas variables

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,15 +1,15 @@
 // 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
-let frase = prompt('Escribe una frase');
-frase = frase.toLowerCase();
-console.log(frase);
-for (let i = 0; i < frase.length; i++) {
+let phrase = prompt('Escribe una frase');
+phrase = phrase.toLowerCase();
+console.log(phrase);
+for (let i = 0; i < phrase.length; i++) {
 	if (
-		frase.substr(i, 1) == 'a' ||
-		frase.substr(i, 1) == 'e' ||
-		frase.substr(i, 1) == 'i' ||
-		frase.substr(i, 1) == 'o' ||
-		frase.substr(i, 1) == 'u'
+		phrase.substr(i, 1) == 'a' ||
+		phrase.substr(i, 1) == 'e' ||
+		phrase.substr(i, 1) == 'i' ||
+		phrase.substr(i, 1) == 'o' ||
+		phrase.substr(i, 1) == 'u'
 	) {
-		document.write(frase.substr(i, 1));
+		document.write(phrase.substr(i, 1));
 	}
 }
